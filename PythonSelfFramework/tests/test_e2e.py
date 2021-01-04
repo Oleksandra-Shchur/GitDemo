5a58ba2b4c7e84d30a91876e0501e5cb921bb391
oleksandra <oleksandra126@gmail.com>
2021-01-04 22:09:10 +0200

    PythonFramework

---
+++ PythonSelfFramework/tests/test_e2e.py
@@ -0,0 +1,47 @@
+import pytest
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+from utilities.BaseClass import BaseClass
+from pageObjects.CheckoutPage import CheckOutPage
+from pageObjects.HomePage import HomePage
+
+
+class TestOne(BaseClass):
+
+    def test_e2e(self):
+        log = self.getLogger()
+        homePage = HomePage(self.driver)
+        checkOutPage = homePage.shopItems()
+        log.info("getting all the card titles")
+        cards = checkOutPage.getCardTitles()
+        i = -1
+        for card in cards:
+            i = i+1
+            cardText = card.text
+            log.info(cardText)
+            if cardText == "Blackberry":
+                checkOutPage.getCardFooter()[i].click()
+
+        self.driver.find_element_by_css_selector("a[class*='btn-primary']").click()
+        confirmpage = checkOutPage.checkOutItems()
+        log.info("Entering country name as ind")
+        self.driver.find_element_by_id("country").send_keys("ind")
+        self.verifyLinkPresence("India")
+
+        self.driver.find_element_by_link_text("India").click()
+        self.driver.find_element_by_xpath("//div[@class='checkbox checkbox-primary']").click()
+        self.driver.find_element_by_css_selector("[type='submit']").click()
+        textMatch = self.driver.find_element_by_css_selector("[class*='alert-success']").text
+        log.info("Text received from application is " + textMatch)
+        assert ("Success! Thank you!" in textMatch)
+
+
+
+
+
+
